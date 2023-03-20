52b96c908933214be48b850dcd3da1a396d0f23c
Saransh Mishra <103825536+SarmisH@users.noreply.github.com>
2023-03-20 13:17:24 +0000

    reset

--- README.md
+++
@@ -1,2 +0,0 @@
-# Saransh Mishra- personal portfolio website
-Introduction portfolio website made using pure HTML, CSS and JS
--- script.js
+++
@@ -1,5 +0,0 @@
-console.log("Script running...")
-document.querySelector('.menu').addEventListener("click", ()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebarclose');
-
-})
